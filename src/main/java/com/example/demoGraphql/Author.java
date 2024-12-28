f8bdf57484edd520f068c24ff5c03d38774d5c38
Netsai <hellonetsai@gmail.com>
2024-12-29 01:35:34 +0200

    Fix data sources issues

--- src/main/java/com/example/demoGraphql/Author.java
+++ src/main/java/com/example/demoGraphql/Author.java
@@ -6,9 +6,8 @@ import java.util.List;
 
 public record Author(String id, String firstName, String lastName) {
     private static List<Author> authors = Arrays.asList(
-    new Author("author-1", "Joshua", "Bloch"),
-     new Author("author-2", "Douglas", "Adams"),
-    new Author("author-3", "Bill", "Bryson")
+     new Author("author-2", "Douglas", "Adams")
+
     );
     public static Author getById(String id) {
         return authors.stream()
